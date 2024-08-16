51a42ac0f00bec04ec5c813b7f71d584aaa48b30
Antonio <antonicut@gmail.com>
2024-08-17 00:16:10 +0200

    Add Ejercicio Practico 1

--- 00-apps-react-pildorasinformaticas/src/App.js
+++ 00-apps-react-pildorasinformaticas/src/App.js
@@ -3,9 +3,8 @@
 //  ***********************************
 
 
-
-import logo from './logo.svg';
 import './App.css';
+import { HeaderComponent } from '././HeaderComponent/jsx/HeaderComponent.jsx';
 import { Titulo } from './components/Titulo.jsx';
 import { UseState } from './components/UseState.jsx';
 import { UseRef } from './components/UseRef.jsx';
@@ -13,8 +12,8 @@ import { ConversorEuroDolar } from './components/ConversorEuroDolar.jsx';
 import { Arrays } from './components/Arrays.jsx';
 import { Contador } from './components/Contador.jsx';
 import { Saludo } from './components/Saludo.jsx';
-import { RenderizadoCondicional }from './components/RenderizadoCondicional.jsx';
-
+import { RenderizadoCondicional } from './components/RenderizadoCondicional.jsx';
+import { EjercicioPractico1 } from './components/EjercicioPractico1.jsx';
 
 
 /**
@@ -38,61 +37,20 @@ const App = () => {
       genero: "masculino"
    }
 
+
+   
+
+
+
    return (
 
       <div className="App">
 
-         <header className="App-header">
-
-            <div className='logo-link'>
-               <img src={logo} className="App-logo" alt="logo" />
-               <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> React </a>
-            </div>
-
-            <h1 className="titulo-proyecto">
-
-               <span className='texto-blanco'> C </span>
-               <span className='texto-azul'>   u </span>
-               <span className='texto-blanco'> r </span>
-               <span className='texto-azul'>   s </span>
-               <span className='texto-blanco'> o </span>
-
-               <span className='texto-azul'>   R </span>
-               <span className='texto-blanco'> e </span>
-               <span className='texto-azul'>   a </span>
-               <span className='texto-blanco'> c </span>
-               <span className='texto-azul'>   t </span>
-
-               <span className='texto-blanco'> </span>
-               <span className='texto-blanco'> - </span>
-               <span className='texto-blanco'> </span>
-
-               <span className='texto-azul'>   P </span>
-               <span className='texto-blanco'> i </span>
-               <span className='texto-blanco'> l </span>
-               <span className='texto-azul'>   d </span>
-               <span className='texto-blanco'> o </span>
-               <span className='texto-azul'>   r </span>
-               <span className='texto-blanco'> a </span>
-               <span className='texto-azul'>   s </span>
-               <span className='texto-blanco'> I </span>
-               <span className='texto-azul'>   n </span>
-               <span className='texto-blanco'> f </span>
-               <span className='texto-azul'>   o </span>
-               <span className='texto-blanco'> r </span>
-               <span className='texto-azul'>   m </span>
-               <span className='texto-blanco'> á </span>
-               <span className='texto-azul'>   t </span>
-               <span className='texto-blanco'> i </span>
-               <span className='texto-azul'>   c </span>
-               <span className='texto-blanco'> a </span>
-               <span className='texto-azul'>   s </span>
-
-            </h1>
-
-         </header>
+         {/* **********  Cabecera del Proyecto  ********** */}
+         <HeaderComponent />
 
 
+         {/* **********  Contenido Principal de la Aplicación  ********** */}
          <main className="App-main">
 
 
@@ -135,6 +93,12 @@ const App = () => {
             {/* **********  7. Renderizado Condicional  **********/}
             <Titulo nameTitulo="----------  7. Renderizado Condicional  ----------" />
             <RenderizadoCondicional usuario2={usuario2} />
+            <hr style={{ marginTop: "50px" }} />
+
+
+            {/* **********  8. Ejercicio Practico 1  **********/}
+            <Titulo nameTitulo="----------  8. Ejercicio Practico 1  ----------" />
+            <EjercicioPractico1 />
             <hr style={{ marginTop: "50px" }} />
 
          </main>
